b08c9adcad4f018b28b4dfa97fe68a0d14d5fc6c
cky <cky@yx.com>
2021-07-29 13:15:23 +0800

    7.29 查看图片视图完成

--- src/views/image-views/image-view-demo.vue
+++ src/views/image-views/image-view-demo.vue
@@ -8,14 +8,28 @@
                 <p class="module-introduced">2.传入imageList数据即可，默认会显示第一张图片</p>
                 <div class="margin-top10">
                     <div style="width: 50px;height: 50px">
-                        <imgView :imageList="imgData"></imgView>
+                        <imgView :imageList="imgData" @click.native="showImage"></imgView>
                     </div>
                 </div>
                 <codemirror class="margin-top20" v-model="imageSlotCode"/>
 
-                <div ref="imgViewBig" class="header-nav-divider">图片放大查看器</div>
-                <p class="module-introduced">1.需要传入coverNavData，图片的list集合，其中包含title,src</p>
+                <div ref="imgViewBig" class="header-nav-divider">图片放大查看器 一般可搭配imgView图片缩略图配合</div>
+                <p class="module-introduced">1.需要传入coverNavData，是图片的list集合，其中包含title,src也可配置title,src</p>
+                <p class="module-introduced">2.navName选中的title名</p>
+                <p class="module-introduced">3.dataKey用于配置的title和src
+                    例如：{
+                        title: 'label',
+                        src: 'url'
+                    }
+                </p>
 
+                <p class="module-introduced">4.coverDialogShow布尔值显示查看弹窗，可配置sync异步关闭弹窗</p>
+
+                <codemirror class="margin-top20" v-model="viewPhotoCode"/>
+
+                <viewPhoto :coverDialogShow.sync="imageShow" :coverNavData="data"></viewPhoto>
+
+                <el-button class="margin-top10" type="primary" size="mini" @click="showImage">查看图片</el-button>
             </div>
         </div>
     </div>
@@ -30,7 +44,15 @@
         data() {
             return {
                 imgData: ['https://img1.baidu.com/it/u=1361135963,570304265&fm=26&fmt=auto&gp=0.jpg'],
+                imageShow: false,
+                data: [
+                    {
+                        title: '示例一',
+                        src: 'https://img2.baidu.com/it/u=1539068888,918756408&fm=26&fmt=auto&gp=0.jpg'
+                    }
+                ],
                 imageSlotCode: imageCode.imageSlotCode,
+                viewPhotoCode: imageCode.viewPhotoCode
             }
         },
         components: {
@@ -48,6 +70,11 @@
                     title: '图片放大查看器'
                 }
             ]);
+        },
+        methods: {
+            showImage() {
+                this.imageShow = true;
+            }
         }
     }
 </script>
--- src/views/image-views/imageCode.js
+++ src/views/image-views/imageCode.js
@@ -1,13 +1,44 @@
 export default  {
     imageSlotCode: `
      <div style="width: 50px;height: 50px">
-         <image-view :imageList="imgData"></image-view>
+         <image-view :imageList="imgData" @click.native="showImage"></image-view>
      </div>
      
      data() {
         return {
             imgData:['https://img1.baidu.com/it/u=1361135963,570304265&fm=26&fmt=auto&gp=0.jpg']
         }
-     }
+     },
+      methods: {
+            showImage() {
+                this.imageShow = true;
+            }
+      }
     `,
+
+    viewPhotoCode: `
+      <viewPhoto :coverDialogShow.sync="imageShow" :coverNavData="data"></viewPhoto>
+      
+      data() {
+        imageShow: false,
+        data: [
+            {
+                title: '示例一',
+                src: 'https://img2.baidu.com/it/u=1539068888,918756408&fm=26&fmt=auto&gp=0.jpg'
+            }
+        
+        ]
+      
+      },
+      methods: {
+          showImage() {
+                this.imageShow = true;
+          }
+      }
+      
+      
+      
+      
+      
+    `
 }
